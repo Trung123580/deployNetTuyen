c6e9a1d0d450fad2ec20a92af6d400c8f012e422
Trung Vương <trungvuong3012@gmail.com>
2023-10-25 14:44:58 +0700

    last

--- src/app.js
+++ src/app.js
@@ -14,11 +14,14 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nettruyencomments',
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 app.get('/', (req, res) => {
   const sql = 'SELECT * FROM users';
