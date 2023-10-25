ad267e95068fad08ccdca522c6b39ae9dcdc0678
Trung Vương <trungvuong3012@gmail.com>
2023-10-25 13:50:08 +0700

    10

---
+++ database.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log('connect to PostgreSQL');
+});
+module.exports = pool;
